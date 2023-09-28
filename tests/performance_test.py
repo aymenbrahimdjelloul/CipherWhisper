f6fa038d0e2cca5006e80b14258086f9254cecf4
Aymen Brahim Djelloul <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2023-09-28 22:24:29 +0100

    Add files via upload

---
+++ tests/performance_test.py
@@ -0,0 +1,29 @@
+# This script implement the performance test to initialize and encrypt and decrypt using CipherWhisper
+# Author : Aymen Brahim Djelloul
+
+import sys
+import os
+from time import perf_counter
+
+# Add the 'src' directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+# Import the CipherWhisper algorithm
+from cipherwhisper import CipherWhisper
+
+# Define encryption key
+KEY = "0123456789"
+# Define
+TEXT = "X" * 1024
+
+# Create CipherWhisper object
+s_time = perf_counter()     # Store the start time for the initializing step
+obj = CipherWhisper(password=KEY)
+print(f"CipherWhisper initialized in : {perf_counter() - s_time:.5f} s")
+
+# Encrypt 1 KB string
+e = obj.encrypt(TEXT)
+print(f"CipherWhisper Encrypt 1 KB of text in : {perf_counter() - s_time:.5f} s")
+# Decrypt 1 KB string
+d = obj.decrypt(e)
+print(f"CipherWhisper Decrypt 1 KB of text in : {perf_counter() - s_time:.5f} s")
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,2 +1,29 @@
-# This implement the regular encryption and decryption process
+# This script implement the regular process to encrypt and decrypt
+# Author : Aymen Brahim Djelloul
 
+import sys
+import os
+
+# Add the 'src' directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+# Import the CipherWhisper algorithm
+from cipherwhisper import CipherWhisper
+
+# Define the encryption key and the text will process
+KEY = "0123456789"
+TEXT = "hello world!"
+
+# Create the CipherWhisper object
+obj = CipherWhisper(password=KEY)
+
+# Encrypt the text
+e = obj.encrypt(TEXT)
+
+# Decrypt the text
+d = obj.decrypt(e)
+
+# Print out the result
+print(f"Original Text : {TEXT}")
+print(f"Encrypted Text : {e}")
+print(f"Decrypted Text : {d}")
